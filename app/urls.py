5476b2b35450f7ec4f12109e99b839d2b9fdfb46
raravilov <raravilov@gmail.com>
2019-05-29 11:53:23 +0300

    fix

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from app import views
 
-# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
-
-
 urlpatterns = [
-    # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
      path('', views.file_list, name='file_list'),
      path('<int:year>-<int:month>-<int:day>/', views.file_list, name='file_list'),
      path('<str:name>/',views.file_content, name='file_content')
