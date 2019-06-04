0741a171db4a1efe7dd006a32ffc3ba923529ba5
raravilov <raravilov@gmail.com>
2019-06-04 22:26:43 +0300

    fix 6. Можно (и нужно) читать из file_names и фильтровать по дате в одном цикле. Тогда и с context’ом не нужно будет мудрить. 7. В file_content необходимо проверять наличие запрашиваемого файла, и при отсутствии выводить ответ 404. 8. Не стоит использовать зарезервированное слово file в качестве переменных.

--- app/views.py
+++ app/views.py
@@ -8,32 +8,26 @@ from app import settings
 def file_list(request, year=None, month=None, day=None):
     template_name = 'index.html'
     file_listing = []
-    out_file_list = []
     file_names = os.listdir(settings.FILES_PATH)
+    if year:
+        url_date = datetime.date(year, month, day)
+    else:
+        url_date = None
     for file in file_names:
         file_dict = {}
         file_dict['name'] = file
         file_data = os.stat('files/' + file)
         file_dict['ctime'] = datetime.date.fromtimestamp(file_data.st_ctime)
         file_dict['mtime'] = datetime.date.fromtimestamp(file_data.st_mtime)
-        file_listing.append(file_dict)
+        if url_date is not None:
+            if file_dict['mtime'] == url_date:
+                file_listing.append(file_dict)
+        else:
+            file_listing.append(file_dict)
     context = {
-        'files': file_listing,
-        'date': None
+        'files': sorted(file_listing, key=lambda k: k['mtime']),
+        'date': url_date
     }
-
-    if year is not None:
-        context.clear()
-        out_file_list = []
-        url_date = datetime.date(year, month, day)
-        for file in file_listing:
-            if file['mtime'] == url_date:
-                out_file_list.append(file)
-        context = {
-            'files': out_file_list,
-            'date': url_date
-        }
-
     return render(request, template_name, context)
 
 
